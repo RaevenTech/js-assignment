2a71db4884b5cde8cb7ade3953215502f627130c
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-03-06 16:35:02 +0000

    Assignment 1,2,3 complete

--- assignment-3/conditionals-loop.js
+++ assignment-3/conditionals-loop.js
@@ -1,2 +1,17 @@
 //  a) Write a JavaScript program that accepts two integers and displays the larger of the two.
+const num1 = 1
+const num2 = 2
+if (num1 > num2) {
+    console.log(num2)
+      } else {
+        console.log(num1)
+      }
 //  b) Write a JavaScript program that checks whether an integer is an even or an odd number.
+
+const int = 7
+
+if (int % 2 === 0) {
+  console.log("is Even")
+} else {
+  console.log("is odd")
+}
