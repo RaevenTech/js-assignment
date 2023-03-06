e2eab3504f89f621538ff07318aa6d0d41d1af3f
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-03-06 12:39:07 +0000

    file tree created

---
+++ assignment-3/conditionals-loop.js
@@ -0,0 +1,2 @@
+//  a) Write a JavaScript program that accepts two integers and displays the larger of the two.
+//  b) Write a JavaScript program that checks whether an integer is an even or an odd number.
