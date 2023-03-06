7f5f2b4faaa39b08fd35cdddacb15fe0f0c4f7e3
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-03-06 19:38:21 +0000

    assignment 4 incomplete

--- assignment-4/rock-paper-scissors.js
+++ assignment-4/rock-paper-scissors.js
@@ -1,1 +1,23 @@
 // Rock Paper Scissors
+let choice = Math.floor(Math.random() * 3)
+
+switch (choice) {
+  case 0:
+    console.log(`rock`)
+    break;
+  case 1:
+    console.log(`paper`)
+    break;
+  case 2:
+  console.log(`scissors`)
+    break;   
+  default:
+      console.log(`Invalid entry`) 
+}
+
+function game() {
+  let data = { 1: "rock", 2: "paper", 3: "scissors" };
+  let random = Math.random() * (1 - 3) + 1;
+  return data.random;
+}
+console.log(game())
